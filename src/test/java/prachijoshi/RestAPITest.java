1a98713ced485928bff4057ac18d765355d31e1b
Prachi <prachijoshi273@gmail.com>
2022-05-03 16:42:42 +0530

    this is commit from India

--- src/test/java/prachijoshi/RestAPITest.java
+++ src/test/java/prachijoshi/RestAPITest.java
@@ -8,12 +8,14 @@ public class RestAPITest {
 	public void methodC()
 	{
 		System.out.println("RESTAPITest test 1");
+		System.out.println("I,m imported from GitHub");
 	}
 
 	@Test
 	public void methodD()
 	{
 		System.out.println("RESTAPITest test 2");
+		System.out.println("I'am imported GitHub2");
 	}
 
 
